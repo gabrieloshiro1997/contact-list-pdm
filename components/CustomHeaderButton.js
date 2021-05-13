e9569becfbbf899e913b8fa4cd9a736e6eecf7c3
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-05-12 21:37:01 -0300

    refactor: change button header color

--- components/CustomHeaderButton.js
+++ components/CustomHeaderButton.js
@@ -10,7 +10,7 @@ const CustomHeaderButton = (props) => {
       {...props}
       IconComponent={Ionicons}
       iconSize={23}
-      color={Platform.OS === 'android' ? 'black' : Colors.primary}
+      color={Platform.OS === 'android' ? 'white' : Colors.primary}
     />
   );
 };
